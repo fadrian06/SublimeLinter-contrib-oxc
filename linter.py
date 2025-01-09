4cde2d055851d90cf16f6e1e47b259708d3378f7
fadrian06 <franyeradriansanchez@gmail.com>
2025-01-09 02:31:03 -0400

    Linter working

--- linter.py
+++ linter.py
@@ -1,10 +1,28 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from re import Match
+
+from SublimeLinter.lint import NodeLinter
+from SublimeLinter.lint.linter import LintMatch
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
+class oxlint(NodeLinter):
     defaults = {
-        'selector': 'source.python'
+        "selector": "source.js"
     }
+
+    cmd = "oxlint --format=unix"
+
+    regex = (
+        r"(?P<filename>\w+\.\w+):"
+        r"(?P<line>\d+):"
+        r"(?P<col>\d+):"
+        r"\s*(?P<message>[\w\s\.\-\=]+\.?)"
+        r"\s*\[(?P<warning>Warning)?(?P<error>Error)?/"
+        r"(?P<code>.+)]"
+    )
+
+    def split_match(self, match: Match) -> LintMatch:
+        groupdict = match.groupdict()
+
+        print(groupdict)
+
+        return super().split_match(match)
